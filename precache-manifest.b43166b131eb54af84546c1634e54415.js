6e52094c76bbdb65e29dcddfff4af6cb7c3a3b19
Marko Sutija <markosutija@gmail.com>
2022-11-18 13:21:29 +0100

    Updates

---
+++ precache-manifest.b43166b131eb54af84546c1634e54415.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d99b5c6107b4c54f9d646427efa5d476",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0d37c39cba72d5a0cc34",
+    "url": "./static/css/main.226ab4d7.chunk.css"
+  },
+  {
+    "revision": "339ed6d4ffabc535884e",
+    "url": "./static/js/2.a3308c7e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.a3308c7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d37c39cba72d5a0cc34",
+    "url": "./static/js/main.68f16920.chunk.js"
+  },
+  {
+    "revision": "cfa2b903832d98937ac2",
+    "url": "./static/js/runtime-main.d67c6988.js"
+  }
+]);
